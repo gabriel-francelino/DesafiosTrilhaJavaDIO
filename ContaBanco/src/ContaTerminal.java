f7744d543e1c48752c3490969ca7e82f14091e2a
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-04-30 22:56:36 -0300

    Criando classe para execução

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws RuntimeException {
+
+        try {
+            Conta conta = criarConta();
+
+            conta.gerarInfo();
+
+        } catch (Exception e) {
+            System.out.println("Erro:" + e.getMessage());
+        }
+    }
+
+    public static Conta criarConta() {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.getDefault());
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+        Utils.validarNome(nomeCliente);
+
+        System.out.println("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+        Utils.validarNumeroConta(numeroConta);
+
+        System.out.println("Digite qual é sua agencia: ");
+        String agencia = scanner.next();
+        Utils.validarAgencia(agencia);
+
+        System.out.println("Digite seu saldo inicial: ");
+        double saldo = scanner.nextDouble();
+        Utils.validarSaldo(saldo);
+
+        scanner.close();
+
+        return new Conta(nomeCliente, numeroConta, agencia, saldo);
+    }
+}
