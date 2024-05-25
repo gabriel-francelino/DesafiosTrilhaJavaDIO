6bed200a57520e2b311d6e4bc81c10cd5eaae99b
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-05-25 14:20:46 -0300

    criando funcionalidades do telefone

--- DesafioModelandoIphone/src/main/java/AparelhoTelefonico.java
+++ DesafioModelandoIphone/src/main/java/AparelhoTelefonico.java
@@ -1,2 +1,35 @@
+import java.util.List;
+
 public class AparelhoTelefonico {
+    List<Contato> contatos;
+
+    public void ligar(Contato contato) {
+        System.out.println("Ligando para " + contato.nome);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo chamada");
+    }
+
+    public void desligar() {
+        System.out.println("Desligando chamada");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz");
+    }
+
+    public void salvarContato(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public void removerContato(Contato contato) {
+        contatos.remove(contato);
+    }
+
+    public void listarContatos() {
+        for (Contato contato : contatos) {
+            System.out.println(contato.toString());
+        }
+    }
 }
--- DesafioModelandoIphone/src/main/java/Iphone.java
+++ DesafioModelandoIphone/src/main/java/Iphone.java
@@ -1,2 +1,13 @@
 public class Iphone {
+    ReprodutorDeMedia reprodutorMusica;
+    ReprodutorDeMedia reprodutorVideo;
+    AparelhoTelefonico aparelhoTelefonico;
+    NavegadorInternet navegadorInternet;
+
+    public Iphone() {
+        reprodutorMusica = new ReprodutorMusical();
+        reprodutorVideo = new ReprodutorDeVideo();
+        aparelhoTelefonico = new AparelhoTelefonico();
+        navegadorInternet = new NavegadorInternet();
+    }
 }
